c314e421d7eaa275869f1432c8f540eb1cbb183f
Yinusa <yinusaamusa137@gmail.com>
2023-06-14 08:38:15 -0500

    finished tut ready to add relevant data

--- EntertainMe/src/App.tsx
+++ EntertainMe/src/App.tsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import GetEventData from './api/getEventData';
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, useMutation } from '@apollo/client';
+import ApolloData from './components/ApolloData';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const client = new ApolloClient({
+    cache: new InMemoryCache(), // will cache the data so that we don't have to fetch it again
+    uri: 'http://localhost:4000/graphql' 
+  })
 
   return (
-    <>
+    <ApolloProvider client={client}>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -26,11 +32,11 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div className="card">
+        <ApolloData />
+   </div>
    <GetEventData />
-    </>
+    </ApolloProvider>
   )
 }
 
