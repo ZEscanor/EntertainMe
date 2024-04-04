1fc71c687a701b17ce996688566df85aff9bc878
Yinusa <yinusaamusa137@gmail.com>
2024-04-03 22:53:30 -0500

    added bugfixes to the ticketmaster map

--- EntertainMe/src/components/Map/Popout.tsx
+++ EntertainMe/src/components/Map/Popout.tsx
@@ -16,6 +16,12 @@ interface PopoutProps {
         name: string;
       }[];
     };
+    dates: {
+      start:{
+        localDate: string;
+        localTime: string;
+      }
+    };
     url: string;
     // Add any other properties your 'event' object might have
   };
@@ -65,12 +71,24 @@ const Popout: React.FC<PopoutProps> = ({event, closeModal, updateDates ,modal}) 
       ]}
       
       >
+       
         <h1>{event.name}</h1>
-        <p>Tickets Price Range - {`$${event.priceRanges[0]?.min} - $${event.priceRanges[0]?.max}  `}</p>
-        <p>Venue: {event._embedded.venues[0]?.name}</p>
-        <p>Dates {event.name}</p>
-        <p>URL - <a href={event.url} target='blank'>{event.url}</a></p>
-      
+
+
+        { event.priceRanges && (
+    <p>Tickets Price Range - {`$${event?.priceRanges[0]?.min} - $${event?.priceRanges[0]?.max}  `}</p>
+)}
+        {event._embedded && event._embedded.venues && (
+    <p>Venue: {event._embedded.venues[0].name}</p>
+)}
+         {event.dates && (
+    <p>Dates: {event.dates.start.localDate}
+    {event.dates.start.localTime}
+    </p>
+)}
+
+
+{ event.url && <p>URL - <a href={event?.url} target='blank'>{event?.url}</a></p> }
       </Modal>
       </>
     ) : (
